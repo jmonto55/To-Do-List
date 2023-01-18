563ac528ad02ed94f6c975c4a2c552768d437708
jmonto55 <jmonto55@gmail.com>
2023-01-17 21:20:13 -0500

    ESlint debugging #4

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
-/* eslint-disable linebreak-style import/no-unresolved */
+/* eslint-disable linebreak-style */
+/* eslint-disable import/no-unresolved */
 
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
