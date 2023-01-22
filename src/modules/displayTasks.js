0dbf39deba8df16d6873f8f634f4e666860352bf
jmonto55 <jmonto55@gmail.com>
2023-01-22 16:43:08 -0500

    Code cleaning

--- src/modules/displayTasks.js
+++ src/modules/displayTasks.js
@@ -4,11 +4,6 @@ if (!taskList) { taskList = []; }
 const displayTasks = () => {
   const listSection = document.querySelector('.tasks_list');
   listSection.innerHTML = '';
-  listSection.innerHTML += `
-    <li class="add_task">
-      <input class="add_to_list" type="text" placeholder="Add to your list..." />
-      <span id="submit_icon" class="material-symbols-outlined">subdirectory_arrow_left</span>
-    </li>`;
   taskList.forEach((e) => {
     let completedCheck = '';
     let styleCheck = '';
@@ -25,10 +20,6 @@ const displayTasks = () => {
         <span class="material-symbols-outlined remove_button">delete</span>
       </li>`;
   });
-  listSection.innerHTML += `
-    <li class="clear_tasks">
-      <button class="clear_all" type="submit">Clear all completed</button>
-    </li>`;
 };
 
 export default displayTasks;
